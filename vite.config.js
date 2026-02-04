153be243effd95a6c4142c05c2407ccfa9509f58
December <jiale.gfi@outlook.com>
2026-02-04 20:48:52 +0800

    Please enter the commit message for your changes. Lines starting

--- vite.config.js
+++ vite.config.js
@@ -70,7 +70,18 @@ export default defineConfig(({ command })=> {
         output: {
           chunkFileNames: 'static/js/[name]-[hash].js',
           entryFileNames: 'static/js/[name]-[hash].js',
-          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+          // 静态资源分拆打包
+          manualChunks (id) {
+            if (id.includes('node_modules')) {
+              if (id.toString().indexOf('.pnpm/') !== -1) {
+                return id.toString().split('.pnpm/')[1].split('/')[0].toString();
+              } else if (id.toString().indexOf('node_modules/') !== -1) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+            }
+          }
+
         }
       },
       sourcemap: false,
